1e4f6bb21fb8f309a0f803bab74efac30eb584f8
Gabriel ML <gabrielmljob@gmail.com>
2025-08-26 14:20:10 +0200

    Initial commit: Java Project structure with Maven & Spring Boot.
    Project setup with "spring initializr":
    - [x] Language: Java 21.
    - [x] Build tool: Maven.
    - [x] Spring Boot version: 3.5.5.
    - [x] Packaging: JAR.
    - [x] Dependencies = ["Spring Web", "H2 Database", "Spring Boot Dev
      Tools", "Spring Data JPA", "MySQL Driver", "Docker Compose Support"];

---
+++ src/main/java/dev/gml/project_support_app/ProjectSupportAppApplication.java
@@ -0,0 +1,13 @@
+package dev.gml.project_support_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSupportAppApplication.class, args);
+	}
+
+}
